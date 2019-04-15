fcf822826d7e460f1439afef506676e8a560c909
Dan <dan.zajac@mac.com>
2019-04-15 20:19:48 +0000

    Adds looping exercises

---
+++ chorus-while.js
@@ -0,0 +1,10 @@
+var chorus = "Lets Dance!"
+var repeat = 0;
+while (repeat < 10) {
+        if (repeat === 3) {
+        console.log("chnge key");
+        }
+        console.log(chorus);
+        repeat++;
+}
+console.log("Until the sun comes up");
